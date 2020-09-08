bd91754494aef4d39eca1cefd87f710124c6fd4e
Fundor333 <fundor333@fundor333.com>
2020-09-08 22:57:28 +0200

    Basic command with click

--- servergrimoire/__main__.py
+++ servergrimoire/__main__.py
@@ -1,30 +1,4 @@
-import click
-
-
-@click.command()
-def hello():
-    print("""
-.d8888b.                                                         
-d88P  Y88b                                                        
-Y88b.                                                             
- "Y888b.    .d88b.  888d888 888  888  .d88b.  888d888             
-    "Y88b. d8P  Y8b 888P"   888  888 d8P  Y8b 888P"               
-      "888 88888888 888     Y88  88P 88888888 888                 
-Y88b  d88P Y8b.     888      Y8bd8P  Y8b.     888                 
- "Y8888P"   "Y8888  888       Y88P    "Y8888  888                 
-                                                                  
-                                                                  
-                                                                  
- .d8888b.          d8b                        d8b                 
-d88P  Y88b         Y8P                        Y8P                 
-888    888                                                        
-888        888d888 888 88888b.d88b.   .d88b.  888 888d888 .d88b.  
-888  88888 888P"   888 888 "888 "88b d88""88b 888 888P"  d8P  Y8b 
-888    888 888     888 888  888  888 888  888 888 888    88888888 
-Y88b  d88P 888     888 888  888  888 Y88..88P 888 888    Y8b.     
- "Y8888P88 888     888 888  888  888  "Y88P"  888 888     "Y8888  
-    """)
-
+from servergrimoire.dispatcher import grimoire
 
 if __name__ =="__main__":
-    hello()
+    grimoire()
--- servergrimoire/dispatcher.py
+++ servergrimoire/dispatcher.py
@@ -1,5 +1,7 @@
 from importlib import import_module
 
+import click
+
 
 def dynamic_import(abs_module_path, class_name):
     module_object = import_module(abs_module_path)
@@ -7,3 +9,36 @@ def dynamic_import(abs_module_path, class_name):
     target_class = getattr(module_object, class_name)
 
     return target_class
+
+
+@click.command()
+@click.option('--count', is_flag=True, help='Number of greetings.')
+@click.option('--name', is_flag=True, help='The person to greet.')
+def grimoire(count, name):
+    hello()
+    print("")
+    print("For help use --help")
+
+
+def hello():
+    print("""
+.d8888b.                                                         
+d88P  Y88b                                                        
+Y88b.                                                             
+ "Y888b.    .d88b.  888d888 888  888  .d88b.  888d888             
+    "Y88b. d8P  Y8b 888P"   888  888 d8P  Y8b 888P"               
+      "888 88888888 888     Y88  88P 88888888 888                 
+Y88b  d88P Y8b.     888      Y8bd8P  Y8b.     888                 
+ "Y8888P"   "Y8888  888       Y88P    "Y8888  888                 
+                                                                  
+                                                                  
+                                                                  
+ .d8888b.          d8b                        d8b                 
+d88P  Y88b         Y8P                        Y8P                 
+888    888                                                        
+888        888d888 888 88888b.d88b.   .d88b.  888 888d888 .d88b.  
+888  88888 888P"   888 888 "888 "88b d88""88b 888 888P"  d8P  Y8b 
+888    888 888     888 888  888  888 888  888 888 888    88888888 
+Y88b  d88P 888     888 888  888  888 Y88..88P 888 888    Y8b.     
+ "Y8888P88 888     888 888  888  888  "Y88P"  888 888     "Y8888  
+    """)
