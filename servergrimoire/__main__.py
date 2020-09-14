99e784b94f587589a5b617024805262df0a0b691
Matteo <matteo.scarpa@alilaguna.it>
2020-09-14 09:00:51 +0200

    Fix merge

--- servergrimoire/__main__.py
+++ servergrimoire/__main__.py
@@ -1,4 +1,8 @@
 from .dispatcher import grimoire
 
+def launcher():
+    grimoire(prog_name='servergrimoire')
+
+
 if __name__ == "__main__":
-    grimoire(prog_name="servergrimoire")
+    launcher()
